58af0a07cf490bbf5a349dc9cfbf3a35c7d175b8
jpmcginty1 <mcginjoh000@mysbisd.org>
2017-01-12 10:20:51 -0600

    Team 6

--- team6.py
+++ team6.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'team six' # Only 10 chars displayed.
-strategy_name = 'Just do it'
-strategy_description = 'Using lots of brainstorming?'
+strategy_name = 'Betray everyone'
+strategy_description = 'You cant gain points if you dont betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,17 +17,13 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if len(my_history)==0: 
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' 
+    else:
+        return 'b'
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
