8d095036ae5aa52d7b1912a35c7cec27c08489f1
wrighrya000 <wrighrya000@mysbisd.org>
2017-01-12 09:52:59 -0600

    Team 1 Info and Strategy

--- team1.py
+++ team1.py
@@ -11,12 +11,14 @@ strategy_name = 'the killer   '
 strategy_description = 'throw them under the bus'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+     if len(my_history)==0: # Betray first round
+        return 'b'
+     elif their_history[-1]=='b' and my_history[-1]=='b':
+        return 'c'
+     else:
+         return'b' 
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
